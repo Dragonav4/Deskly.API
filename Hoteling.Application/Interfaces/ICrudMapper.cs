86d85060f45cf71e51b8e3de2ab7710ac50f1d68
Danyil Danilian <daniliandan100500@gmail.com>
2026-02-04 23:39:26 +0100

    fix(mappers): update async method

--- Hoteling.Application/Interfaces/ICrudMapper.cs
+++ Hoteling.Application/Interfaces/ICrudMapper.cs
@@ -6,8 +6,8 @@ namespace Hoteling.Application.Interfaces;
 
 public interface ICrudMapper<TDomain, in TCreateDto, TViewDto> where TViewDto : TCreateDto, IHasId
 {
-    ActionListView<TViewDto> MapDomainModelsToListView(IEnumerable<TDomain> domains, int totalCount, ClaimsPrincipal user);
+    Task<ActionListView<TViewDto>> MapDomainModelsToListView(IEnumerable<TDomain> domains, int totalCount, ClaimsPrincipal user);
     TDomain MapCreateDtoToDomain(TCreateDto createDto);
     TDomain MapViewToDomain(Guid id, TViewDto viewDto);
-    TViewDto MapDomainToView(TDomain domain, ClaimsPrincipal user);
+    Task<TViewDto> MapDomainToView(TDomain domain, ClaimsPrincipal user);
 }
